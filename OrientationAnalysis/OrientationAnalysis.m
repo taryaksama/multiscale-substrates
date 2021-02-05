5bef1d043def9058849d0a35a7988a051996b0da
taryaksama <t.aryaksama@hotmail.fr>
2021-02-05 11:30:28 +0100

    Modified part 2. orientation vs. width

--- OrientationAnalysis/OrientationAnalysis.m
+++ OrientationAnalysis/OrientationAnalysis.m
@@ -45,9 +45,17 @@ for i = 1:length(d)
     end
 end
 
-%% 1. middle angle vs. width
+%% 2. orientation vs. width
 
-Angw = NaN*ones(1,3,length(d));
+pos = 'middle'; % look at angles in the middle of stripes
+
+% load orientation data
+switch pos
+    case 'middle'
+        Angw = NaN*ones(1,3,length(d));
+    case 'edge'
+        Angw = NaN*ones(1,3,2*length(d));
+end
 for i = 1:length(d)
     clc; disp([num2str(i),'/',num2str(length(d))]);
     if d(i).isdir==0
@@ -55,73 +63,46 @@ for i = 1:length(d)
         load([pathname,'\analysis\orientation data\',strrep(d(i).name,'.tif','.mat')]);
         
         px2mic = setpx2mic(d(i).name,'orientation');
-        Angw(1,1,i) = data_orient.Width*px2mic;
-        Angw(1,2:3,i) = data_orient.AverageAngle.HtimeAverage.L(end,:);
-        
+        Angw(1,1,i) = data_orient.Width * px2mic;
+        switch pos
+            case 'middle'
+                Angw(1,2:3,i) = data_orient.AverageAngle.HtimeAverage.L(end,:);
+            case 'edge'
+                Angw(1,2:3,i) = data_orient.AverageAngle.HtimeAverage.L(1,:);
+                Angw(1,2:3,length(d)+i) = data_orient.AverageAngle.HtimeAverage.R(1,:);
+            otherwise
+                disp('specify position for analysis: middle/edge');
+                break
+        end
     end
 end
 Angw(:,:,find(prod((isnan(Angw(:,:,:))))==1)) = [];
 
 % binned data
-% one branch
 Angw1_bin = NaN * ones(length(bw)-1,3);
-for i = 1:length(bw)-1
-    Angw1 = squeeze(Angw)'; Angw1(:,2) = abs(Angw1(:,2));
-    [Angw1_bin(i,1), Angw1_bin(i,2), Angw1_bin(i,3)] = vecBin1(Angw1, [bw(i) bw(i+1)], ep, 'angle');
-end
-Angw1_bin(find(prod(isnan(Angw1_bin)==1,2)),:) = [];
-
-% two branches
 Angw2_bin = NaN * ones(length(bw)-1,6);
 for i = 1:length(bw)-1
-    Angw2 = squeeze(Angw)';
-    Angw2 = Angw2(find((Angw2(:,1)>=bw(i)+ep).*(Angw2(:,1)<bw(i+1)+ep)),:);
-    if isempty(Angw2)==0
-        if size(Angw2,1)==1
-            Angw2_bin(i,:) = [Angw2(:,1), NaN, Angw2(:,2), Angw2(:,3), Angw2(:,2), Angw2(:,3)];
+    %one branch
+        Angw1 = squeeze(Angw)'; Angw1(:,2) = abs(Angw1(:,2));
+        [Angw1_bin(i,1), Angw1_bin(i,2), Angw1_bin(i,3)] = vecBin1(Angw1, [bw(i) bw(i+1)], ep, 'angle');
+    
+    %two branches (using von Mises distributions separation)
+        Angw2 = squeeze(Angw)';
+        Angw2 = Angw2(find((Angw2(:,1)>=bw(i)+ep).*(Angw2(:,1)<bw(i+1)+ep)),:);
+        if isempty(Angw2)==0
+            if size(Angw2,1)==1
+                Angw2_bin(i,:) = [Angw2(:,1), NaN, Angw2(:,2), Angw2(:,3), Angw2(:,2), Angw2(:,3)];
+            else
+                Angw2_bin(i,:) = fitBimod(Angw2(:,1:2), 'off');
+            end
         else
-            Angw2_bin(i,:) = fitBimod(Angw2(:,1:2), 'off');
+            continue
         end
-    else
-        continue
-    end
 end
+Angw1_bin(find(prod(isnan(Angw1_bin)==1,2)),:) = [];
 Angw2_bin(find(prod(isnan(Angw2_bin)==1,2)),:) = [];
 
-% figures
-switch fig
-    case 'on'
-        
-        % angle vs. width (double branch)
-        figure(11);  clf;
-        subplot(2,1,1); hold on;
-        %         errorbar(squeeze(Angw(:,1,:)),squeeze(Angw(:,2,:)),squeeze(Angw(:,3,:)),'ko');
-        plot(squeeze(Angw(:,1,:)),squeeze(Angw(:,2,:)),'ko');
-        errorbar(Angw2_bin(:,1), Angw2_bin(:,3), Angw2_bin(:,4),'rs','MarkerSize',10); % branch 1
-        errorbar(Angw2_bin(:,1), Angw2_bin(:,5), Angw2_bin(:,6),'bs','MarkerSize',10); % branch 2
-        plot([bw+ep ; bw+ep],[-90 ; 90]*ones(1,length(bw)),'k--');
-        plot([0 1200], [-90 -90], 'k-'); plot([0 1200], [90 90], 'k-'); plot([0 1200], [0 0], 'k-');
-        axis([0 1200 -100 100]);
-        title('Angle vs. stripe width (two branches)'); xlabel('width of stripe'); ylabel('cell angle (degrees)');
-        
-        % angle vs. width (absolute value)
-        figure(11);
-        subplot(2,1,2); hold on;
-        plot([bw+ep ; bw+ep],[0 ; 90]*ones(1,length(bw)),'k--');
-        plot(squeeze(Angw(:,1,:)),abs(squeeze(Angw(:,2,:))),'ko');
-        errorbar(Angw1_bin(:,1),Angw1_bin(:,2),Angw1_bin(:,3),'rs','MarkerSize',10)
-        plot([0 1200], [0 0], 'k-'); plot([0 1200], [90 90], 'k-');
-        axis([0 1200 -10 100]);
-        title(['Angle vs. stripe width (binning = ',num2str(bw_sz),' um)']); xlabel('width of stripe'); ylabel('cell angle (degrees)');
-        
-        figure(11);
-        savefig([pathname,'\analysis\figures\',exp_name,'_middle_angle_vs_width.fig']);
-        
-    otherwise
-        0;
-end
-
-%% 2. profiles
+%% 3. profiles
 
 AngX = NaN*ones(100,3,length(d));
 for i = 1:length(d)
@@ -147,50 +128,50 @@ for i = 1:length(bw)-1
     AngX_bin{i,1} = {[num2str(bw(i)),' - ',num2str(bw(i+1))]};
     [AngX_bin{i,2}, AngX_bin{i,3}, AngX_bin{i,4}, AngX_bin{i,5}] = vecBin2(AngX(:,1:2,:), [bw(i) , bw(i+1)], bx_sz, ep, 'abs', 'Nematic Angle');
 end
-%%
+
 % figures
-switch fig
-    case 'on'
+% switch fig
+%     case 'on'
         
-        figure(21); clf;
-        for i = 1:size(AngX_bin,1)
-            subplot(4,4,i); hold on;
-            profile = AngX_bin{i,3};
-            if isempty(profile)==1
-                continue
-            end
-            plot(profile(:,1),profile(:,2),'ko');
-            xbin = AngX_bin{i,4}(:,1); ybin = AngX_bin{i,4}(:,2); sbin = AngX_bin{i,4}(:,3);
-            plot(xbin,ybin,'rs-','MarkerSize',10);
-            title(AngX_bin{i,1}); xlabel('position in stripes'); ylabel('cellular angle');
-        end
-        
-        figure(22); clf;
-        cmap = colormap(cool(size(AngX_bin,1)));
-        for i = 1:size(AngX_bin,1)
-            if isempty(AngX_bin{i,5})==1
-                continue
-            end
-            
-            subplot(1,2,1); hold on;
-             xbin = AngX_bin{i,4}(:,1); ybin = AngX_bin{i,4}(:,2); sbin = AngX_bin{i,4}(:,3);
-            plot(xbin,ybin,'o-','Color',cmap(i,:));
-            title(AngX_bin{i,1}); xlabel('position in stripes'); ylabel('cellular angle');
-             axis([-600 600 -10 100]); axis square;
-            
-            subplot(1,2,2); hold on;
-            xbin = AngX_bin{i,5}(:,1); ybin = AngX_bin{i,5}(:,2);
-            plot(xbin,ybin,'o-','Color',cmap(i,:));
-            title('normalized profiles'); xlabel('X/w'); ylabel('cellular angle');
-            axis([-0.6 0.6 -10 100]); axis square;
-        end
-        
-%         figure(21); savefig([pathname,'\analysis\figures\',exp_name,'_angle_profile_all_points.fig']);
-%         figure(22); savefig([pathname,'\analysis\figures\',exp_name,'_angle_profile_normalized.fig']);
-        
-    otherwise
-        0;
-end
+%         figure(21); clf;
+%         for i = 1:size(AngX_bin,1)
+%             subplot(4,4,i); hold on;
+%             profile = AngX_bin{i,3};
+%             if isempty(profile)==1
+%                 continue
+%             end
+%             plot(profile(:,1),profile(:,2),'ko');
+%             xbin = AngX_bin{i,4}(:,1); ybin = AngX_bin{i,4}(:,2); sbin = AngX_bin{i,4}(:,3);
+%             plot(xbin,ybin,'rs-','MarkerSize',10);
+%             title(AngX_bin{i,1}); xlabel('position in stripes'); ylabel('cellular angle');
+%         end
+%         
+%         figure(22); clf;
+%         cmap = colormap(cool(size(AngX_bin,1)));
+%         for i = 1:size(AngX_bin,1)
+%             if isempty(AngX_bin{i,5})==1
+%                 continue
+%             end
+%             
+%             subplot(1,2,1); hold on;
+%              xbin = AngX_bin{i,4}(:,1); ybin = AngX_bin{i,4}(:,2); sbin = AngX_bin{i,4}(:,3);
+%             plot(xbin,ybin,'o-','Color',cmap(i,:));
+%             title(AngX_bin{i,1}); xlabel('position in stripes'); ylabel('cellular angle');
+%              axis([-600 600 -10 100]); axis square;
+%             
+%             subplot(1,2,2); hold on;
+%             xbin = AngX_bin{i,5}(:,1); ybin = AngX_bin{i,5}(:,2);
+%             plot(xbin,ybin,'o-','Color',cmap(i,:));
+%             title('normalized profiles'); xlabel('X/w'); ylabel('cellular angle');
+%             axis([-0.6 0.6 -10 100]); axis square;
+%         end
+%         
+% %         figure(21); savefig([pathname,'\analysis\figures\',exp_name,'_angle_profile_all_points.fig']);
+% %         figure(22); savefig([pathname,'\analysis\figures\',exp_name,'_angle_profile_normalized.fig']);
+%         
+%     otherwise
+%         0;
+% end
 
 %% 3. edges and coherence length
 
@@ -315,8 +296,130 @@ for i = 1:size(AngXe_bin,1)
 end
 
 % figures
+% switch fig
+%     case 'on'
+%         
+%         figure(31); clf;
+%         cmap = colormap(cool(max(max(AngXe(:,1,:)))));
+%         for i=1:size(AngXe,3)
+%             subplot(2,2,1); hold on;
+%             plot(AngXe(:,2,i),AngXe(:,3,i),'-','Color',cmap(max(AngXe(:,1,i)),:));
+%             title('left edge');
+%             subplot(2,2,3); hold on;
+%             plot(AngXe(:,1,i)./(2*max(AngXe(:,1,i))),AngXe(:,3,i),'-','Color',cmap(max(AngXe(:,1,i)),:));
+%             title('left edge (normaized width)');
+%             subplot(2,2,2); hold on;
+%             plot(AngXe(:,2,i),AngXe(:,5,i),'-','Color',cmap(max(AngXe(:,1,i)),:));
+%             title('right edge');
+%             subplot(2,2,4); hold on;
+%             plot(AngXe(:,1,i)./(2*max(AngXe(:,1,i))),AngXe(:,5,i),'-','Color',cmap(max(AngXe(:,1,i)),:));
+%             title('right edge (normalized width)');
+%             
+%         end
+%         
+%         figure(32); clf;
+%         cmap = colormap(cool(size(AngXe_bin,1)));
+%         subplot(2,1,1);  hold on;
+%         for i = 1:size(AngXe_bin,1)
+%             if isempty(AngXe_bin{i,3})==1
+%                 continue
+%             end
+%             xbinL = AngXe_bin{i,3}(:,1); ybinL = AngXe_bin{i,3}(:,2);
+%             xbinR = AngXe_bin{i,6}(:,1); ybinR = AngXe_bin{i,6}(:,2);
+%             plot(xbinL,ybinL,'o-','Color',cmap(i,:));
+%             plot(xbinR,ybinR,'s-','Color',cmap(i,:));
+%             title('edge profiles'); xlabel('X'); ylabel('cellular angle');
+%         end
+%         subplot(2,1,2);  hold on;
+%         for i = 1:size(AngXe_bin,1)
+%             if isempty(AngXe_bin{i,4})==1
+%                 continue
+%             end
+%             xbinL = AngXe_bin{i,4}(:,1); ybinL = AngXe_bin{i,4}(:,2);
+%             xbinR = AngXe_bin{i,7}(:,1); ybinR = AngXe_bin{i,7}(:,2);
+%             plot(xbinL,ybinL,'o-','Color',cmap(i,:));
+%             plot(xbinR,ybinR,'s-','Color',cmap(i,:));
+%             title('normalized edge profiles'); xlabel('X/w'); ylabel('cellular angle');
+%             axis([0 0.5 -10 100]);
+%         end
+%         
+%         figure(33); clf; hold on;
+% %         subplot(1,2,1); hold on;
+%         plot(lambda(:,1),lambda(:,2),'ko'); plot(lambda(:,1),lambda(:,4),'ks');
+%         title('all points'), xlabel('stripe width (um)'); ylabel('\lambda_{\theta}');
+% %         subplot(1,2,2); hold on;
+%         plot(lambda_bin(:,1),lambda_bin(:,2),'ro-'); plot(lambda_bin(:,1),lambda_bin(:,4),'rs-');
+% %         title('on binned profiles'), xlabel('stripe width (um)'); ylabel('\lambda_{\theta}');
+%         legend('left edge', 'right edge');
+%         
+%         figure(31); savefig([pathname,'\analysis\figures\',exp_name,'_edge_profile_all_points.fig']);
+%         figure(32); savefig([pathname,'\analysis\figures\',exp_name,'_edge_profile_binned.fig']);
+%         figure(33); savefig([pathname,'\analysis\figures\',exp_name,'_coherence_length.fig']);
+%         
+%     otherwise
+%         0;
+%         
+% end
+
+%% FIGURES
+
 switch fig
     case 'on'
+        
+        % angle vs. width (double branch)
+        figure(11);  clf;
+        subplot(2,1,1); hold on;
+        %         errorbar(squeeze(Angw(:,1,:)),squeeze(Angw(:,2,:)),squeeze(Angw(:,3,:)),'ko');
+        plot(squeeze(Angw(:,1,:)),squeeze(Angw(:,2,:)),'ko');
+        errorbar(Angw2_bin(:,1), Angw2_bin(:,3), Angw2_bin(:,4),'rs','MarkerSize',10); % branch 1
+        errorbar(Angw2_bin(:,1), Angw2_bin(:,5), Angw2_bin(:,6),'bs','MarkerSize',10); % branch 2
+        plot([bw+ep ; bw+ep],[-90 ; 90]*ones(1,length(bw)),'k--');
+        plot([0 1200], [-90 -90], 'k-'); plot([0 1200], [90 90], 'k-'); plot([0 1200], [0 0], 'k-');
+        axis([0 1200 -100 100]);
+        title('Angle vs. stripe width (two branches)'); xlabel('width of stripe'); ylabel('cell angle (degrees)');
+        
+        % angle vs. width (absolute value)
+        figure(11);
+        subplot(2,1,2); hold on;
+        plot([bw+ep ; bw+ep],[0 ; 90]*ones(1,length(bw)),'k--');
+        plot(squeeze(Angw(:,1,:)),abs(squeeze(Angw(:,2,:))),'ko');
+        errorbar(Angw1_bin(:,1),Angw1_bin(:,2),Angw1_bin(:,3),'rs','MarkerSize',10)
+        plot([0 1200], [0 0], 'k-'); plot([0 1200], [90 90], 'k-');
+        axis([0 1200 -10 100]);
+        title(['Angle vs. stripe width (binning = ',num2str(bw_sz),' um)']); xlabel('width of stripe'); ylabel('cell angle (degrees)');
+        
+         figure(21); clf;
+        for i = 1:size(AngX_bin,1)
+            subplot(4,4,i); hold on;
+            profile = AngX_bin{i,3};
+            if isempty(profile)==1
+                continue
+            end
+            plot(profile(:,1),profile(:,2),'ko');
+            xbin = AngX_bin{i,4}(:,1); ybin = AngX_bin{i,4}(:,2); sbin = AngX_bin{i,4}(:,3);
+            plot(xbin,ybin,'rs-','MarkerSize',10);
+            title(AngX_bin{i,1}); xlabel('position in stripes'); ylabel('cellular angle');
+        end
+        
+        figure(22); clf;
+        cmap = colormap(cool(size(AngX_bin,1)));
+        for i = 1:size(AngX_bin,1)
+            if isempty(AngX_bin{i,5})==1
+                continue
+            end
+            
+            subplot(1,2,1); hold on;
+             xbin = AngX_bin{i,4}(:,1); ybin = AngX_bin{i,4}(:,2); sbin = AngX_bin{i,4}(:,3);
+            plot(xbin,ybin,'o-','Color',cmap(i,:));
+            title(AngX_bin{i,1}); xlabel('position in stripes'); ylabel('cellular angle');
+             axis([-600 600 -10 100]); axis square;
+            
+            subplot(1,2,2); hold on;
+            xbin = AngX_bin{i,5}(:,1); ybin = AngX_bin{i,5}(:,2);
+            plot(xbin,ybin,'o-','Color',cmap(i,:));
+            title('normalized profiles'); xlabel('X/w'); ylabel('cellular angle');
+            axis([-0.6 0.6 -10 100]); axis square;
+        end
         
         figure(31); clf;
         cmap = colormap(cool(max(max(AngXe(:,1,:)))));
@@ -375,12 +478,14 @@ switch fig
         figure(32); savefig([pathname,'\analysis\figures\',exp_name,'_edge_profile_binned.fig']);
         figure(33); savefig([pathname,'\analysis\figures\',exp_name,'_coherence_length.fig']);
         
+
+        
     otherwise
         0;
-        
 end
 
-%% 4. saving
+
+%% SAVING
 
 % Angle_width.points = Angw;
 % Angle_width.bin = Angw_bin;
