001bfd2ebccf768908a1b36a14ab025a277407d8
taryaksama <t.aryaksama@hotmail.fr>
2021-02-07 17:34:27 +0100

    Commented PIVAnalysis.m (until 1.convergent flows)

--- PIVAnalysis/PIVAnalysis.m
+++ PIVAnalysis/PIVAnalysis.m
@@ -1,55 +1,54 @@
-% Description:
-% This script is made to compute data of velocities on adhesive
-% stripes containing cells
+% For analysis of PIV of nematic cells in stripes - general code
+% (developed during the thesis of Thibault Aryaksama)
 %
-% Main results:
-%       1. convergent flows: profiles, max value
-%       2. shear flows: profiles, max value, friction length
-%       3. velocity norm: profile, mean value
-%       4. flow direction: profile, coherence length
+% INPUT:
+%   * .tif images (uint 8 or uint16)
+%
+% ANALYSIS:
+%   * 0. parameters setting
+%   * 1. general PIV analysis for each FOV (histograms, over time,
+%   averaged, ...)
+%   * 2. convergent flows (along X-axis)
+%   * 3. shear flows (along Y-axis)
+%   * 4. flow direction
+%   * 5. norm2
+%   * SAVING & FIGURES
+
+%% 0. set parameters of experiment
 
 clear all
 clc
 
+addpath('\functions');
+fig = 'on'; % display figures
+exp_name = input('Experiment name ? \n','s'); % enter experiment name
+
+% binning parameters (width range)
+bw_sz = 100; % width increment
+bw = 0:bw_sz:1200; % width range
+ep = 0; % origin shift
+bx_sz = 20; % X-value increment (for profiles binning)
+
+% set and create analysis folder
 pathname = 'G:\ANALYSIS\transition stripes abrasions\C2C12 stripes abrasions\defect free';
 mkdir([pathname,'\analysis']);
 mkdir([pathname,'\analysis\figures']);
 d = dir([pathname,'\images']);
 
-stripes_size = 'large';
-fig = 'on';
-
-exp_name = input('Experiment name ? \n','s');
-
-% binning parameters
-switch stripes_size
-    case 'large'
-        bw_sz = 100;
-        bw = 0:bw_sz:1200;
-        ep = 0;
-        bx_sz = 20;
-        
-    case 'small'
-        bw_sz = 10;
-        bw = 0:bw_sz:150;
-        ep = 2;
-        bx_sz = 10;
-end
-
-%% 0. compute PIV profiles
+%% 1. computes PIV
 
 dw = 32; % size of ROI
 r = 0.5; % overlap of ROIs
-dt = 0.25; % 4 frames per hour
+dt = 0.25; % 1/frames per hour
 
 for i = 1:length(d)
     clc; disp([num2str(i),'/',num2str(length(d))]);
     if d(i).isdir==0
-        WritePivFiles_v092020(pathname, d(i).name, dw, r, dt, 'single', 'off');
+        WritePIVFiles(pathname, d(i).name, dw, r, dt, 'single', 'off');
     end
 end
 
-%% 1. convergent flows
+%% 2. convergent flows
 
 % profile
 Xu = NaN*ones(100,3,length(d));
@@ -82,7 +81,7 @@ for i = 1:length(bw)-1
     [Xu_bin{i,2}, Xu_bin{i,3}, Xu_bin{i,4}, Xu_bin{i,5}] = vecBin2(Xu, [bw(i) , bw(i+1)], bx_sz, ep, 'achiral', 'XY');
     [Xu_bin{i,6}(:,1), Xu_bin{i,6}(:,2), Xu_bin{i,6}(:,3)] = vecBin1(unorm, [bw(i) bw(i+1)], ep, 'XY');
 end
-%%
+
 %profil length
 lambda_u = NaN*ones(size(Xu,3),3);
 for i = 1:size(Xu,3)
@@ -150,63 +149,8 @@ lambda_u2 = reshape(lambda_u(:,[1,4,2,5,3,6]),[],3);
 lambda_u2 = sortrows(lambda_u2,1);
 lambda_u2(find(isnan(lambda_u2(:,2))==1),:) = [];
 lambda_u2(find(lambda_u2(:,3)<0.7),:)=[];
-%%
-% figure
-switch fig
-    case 'on'
-        
-        %profiles
-        figure(11); clf;
-        for i = 1:size(Xu_bin,1)
-            subplot(4,4,i); hold on;
-            profile = Xu_bin{i,3};
-            if isempty(profile)==1
-                continue
-            end
-            plot(profile(:,1),profile(:,2),'ko');
-            xbin = Xu_bin{i,4}(:,1); ybin = Xu_bin{i,4}(:,2); sbin = Xu_bin{i,4}(:,3);
-            plot(xbin,ybin,'rs-','MarkerSize',10);
-            title(Xu_bin{i,1}); xlabel('position in stripes'); ylabel('cellular angle');
-        end
-        
-        %profiles (normalized)
-        figure(12); clf;
-        cmap = colormap(cool(size(Xu_bin,1)));
-        for i = 1:size(Xu_bin,1)
-            if isempty(Xu_bin{i,4})==1
-                continue
-            end
-            subplot(1,2,1);  hold on;
-            xbin = Xu_bin{i,4}(:,1); ybin = Xu_bin{i,4}(:,2);
-            plot(xbin,ybin,'o-','Color',cmap(i,:));
-            title('normalized profiles'); xlabel('X/w'); ylabel('velocity (um/hr)');
-            axis([-600 600 -10 10]); axis square;
-            subplot(1,2,2);  hold on;
-            xbin = Xu_bin{i,5}(:,1); ybin = Xu_bin{i,5}(:,2);
-            plot(xbin,ybin,'o-','Color',cmap(i,:));
-            title('normalized profiles'); xlabel('X/w'); ylabel('velocity (um/hr)');
-            axis([-0.5 0.5 -10 10]); axis square;
-        end
-        
-        %norm
-        figure(13); clf; hold on;
-        plot(unorm(:,1), unorm(:,2), 'ko');
-        unorm_bin = reshape([Xu_bin{:,6}]',3,[]);
-        unorm_bin(:,find(prod(isnan(unorm_bin),1)==1))=[];
-        plot(unorm_bin(1,:), unorm_bin(2,:), 'ro-', 'MarkerSize', 10);
-        axis([0 1200 0 10]);
-        title('convergent flow norm2'), xlabel('stripe width (um)'); ylabel('velocity (um^2/hr^2)');
-        legend({'all points', 'binned'});
-        
-        figure(11); savefig([pathname,'\analysis\figures\',exp_name,'_convergent_flow_profile_allpoints.fig']);
-        figure(12); savefig([pathname,'\analysis\figures\',exp_name,'_convergent_flow_profile_normalized.fig']);
-        figure(13); savefig([pathname,'\analysis\figures\',exp_name,'_convergent_flow_norm.fig']);
-        
-    otherwise
-        0;
-end
 
-%% 2. shear flows
+%% 3. shear flows
 
 % profile
 Xv = NaN*ones(100,3,length(d));
@@ -239,7 +183,7 @@ for i = 1:length(bw)-1
     [Xv_bin{i,6}(:,1), Xv_bin{i,6}(:,2), Xv_bin{i,6}(:,3)] = vecBin1(vnorm(:,[1 2]), [bw(i) bw(i+1)], ep, 'XY');
     [Xv_bin{i,7}(:,1), Xv_bin{i,7}(:,2), Xv_bin{i,7}(:,3)] = vecBin1(vnorm(:,[1 3]), [bw(i) bw(i+1)], ep, 'XY');
 end
-%%
+
 %screening length
 ft = fittype('b*exp(-(x-x0)/l)','coefficients',{'b','x0','l'});
 lambda_v = NaN*ones(size(Xv,3),5);
@@ -300,7 +244,7 @@ for i = 1:size(Xv,3)
 
     
 end
-%%
+
 lambda_v = sortrows(lambda_v,1);
 
 lambda_v2 = [lambda_v(:,1:4) ; lambda_v(:,5:8)];
@@ -309,74 +253,8 @@ lambda_v2 = [lambda_v(:,1:4) ; lambda_v(:,5:8)];
 lambda_v2 = sortrows(lambda_v2,1);
 lambda_v2(find(isnan(lambda_v2(:,2))==1),:) = [];
 lambda_v2(find(lambda_v2(:,3)<0.8),:)=[];
-%%
-%figure
-switch fig
-    case 'on'
-        
-        %profiles
-        figure(21); clf;
-        for i = 1:size(Xv_bin,1)
-            subplot(4,4,i); hold on;
-            profile = Xv_bin{i,3};
-            if isempty(profile)==1
-                continue
-            end
-            plot(profile(:,1),profile(:,2),'ko');
-            xbin = Xv_bin{i,4}(:,1); ybin = Xv_bin{i,4}(:,2); sbin = Xv_bin{i,4}(:,3);
-            plot(xbin,ybin,'rs-','MarkerSize',10);
-            title(Xv_bin{i,1}); xlabel('position in stripes'); ylabel('cellular angle');
-        end
-        
-        %profiles (normalized)
-        figure(22); clf; 
-        cmap = colormap(cool(size(Xv_bin,1)));
-        for i = 1:size(Xv_bin,1)
-            if isempty(Xv_bin{i,5})==1
-                continue
-            end
-             subplot(1,2,1); hold on;
-            xbin = Xv_bin{i,4}(:,1); ybin = Xv_bin{i,4}(:,2);
-            plot(xbin,ybin,'o-','Color',cmap(i,:));
-            title('profiles'); xlabel('X/w'); ylabel('velocity (um/hr)');
-            axis([-600 600 -10 10]); axis square;
-            
-            subplot(1,2,2); hold on;
-            xbin = Xv_bin{i,5}(:,1); ybin = Xv_bin{i,5}(:,2);
-            plot(xbin,ybin,'o-','Color',cmap(i,:));
-            title('normalized profiles'); xlabel('X/w'); ylabel('velocity (um/hr)');
-            axis([-0.5 0.5 -10 10]); axis square;
-        end
-        
-        %norm
-        figure(23); clf; hold on;
-        plot(vnorm(:,1), vnorm(:,2), 'ko');
-        vnorm_bin = reshape([Xv_bin{:,6}]',3,[]);
-        vnorm_bin(:,find(prod(isnan(vnorm_bin),1)==1))=[];
-        plot(vnorm_bin(1,:), vnorm_bin(2,:), 'ro-', 'MarkerSize', 10);
-        axis([0 1200 0 10]);
-        title('shear flow norm2'), xlabel('stripe width (um)'); ylabel('velocity (um^2/hr^2)');
-        legend({'all points', 'binned'});
-        
-%         figure(24); clf; hold on;
-%         plot(lambda_v(:,1),lambda_v(:,2),'ko'); plot(lambda_v(:,1),lambda_v(:,4),'ks');
-%         title('friction length'), xlabel('stripe width (um)'); ylabel('\lambda_{\theta}');
-%         lambdav_bin = reshape([Xv_bin{:,6}]',5,[]);
-%         lambdav_bin(:,find(prod(isnan(lambdav_bin),1)==1))=[];
-%         plot(lambdav_bin(1,:), lambdav_bin(2,:), 'ro-', 'MarkerSize', 10);
-%         plot(lambdav_bin(1,:), lambdav_bin(4,:), 'rs-', 'MarkerSize', 10);
-%         legend('left edge', 'right edge');
-        
-        figure(21); savefig([pathname,'\analysis\figures\',exp_name,'_shear_flow_profile_allpoints.fig']);
-        figure(22); savefig([pathname,'\analysis\figures\',exp_name,'_shear_flow_profile_normalized.fig']);
-        figure(23); savefig([pathname,'\analysis\figures\',exp_name,'_shear_flow_norm.fig']);
-        figure(24); savefig([pathname,'\analysis\figures\',exp_name,'_shear_flow_friction_length.fig']);
-        
-    otherwise
-        0;
-end
 
-%% 3. velocity direction
+%% 4. velocity direction
 
 % profile
 Xang = NaN*ones(100,2,length(d));
@@ -439,9 +317,170 @@ for i = 1:size(Xang_bin,1)
     [Xang_bin{i,5}(:,4) , Xang_bin{i,5}(:,5)]  = fitCoherenceLength(xfit, yfitR, 'off');
 end
 
-%figure
+%% 5. velocity norm
+
+% profile
+XV = NaN*ones(100,2,length(d));
+Vnorm = NaN*ones(length(d),2);
+for i = 1:length(d)
+    clc; disp([num2str(i),'/',num2str(length(d))]);
+    if d(i).isdir==0
+        
+        load([pathname,'\analysis\velocity data\',strrep(d(i).name,'.tif','.mat')]);
+        
+        px2mic = setpx2mic(d(i).name,'PIV');
+        x = (data.Grid - data.Width/2) * px2mic;
+        XV(1:length(x),1,i) = x;
+        XV(1:length(x),2,i) = data.Profile.V  * px2mic;
+        
+        Vnorm(i,1) = data.Width * px2mic;
+        Vnorm(i,2) = data.Norm2.XYT.V * px2mic^2;
+        
+    end
+end
+XV(:,:,find(prod(prod(isnan(XV(:,:,:)),2))==1)) = [];
+Vnorm(find(prod(isnan(Vnorm(:,:)),2)==1),:) = [];
+
+for i = 1:length(bw)-1
+    XV_bin{i,1} = {[num2str(bw(i)),' - ',num2str(bw(i+1))]};
+    [XV_bin{i,2}, XV_bin{i,3}, XV_bin{i,4}] = vecBin2(XV, [bw(i) , bw(i+1)], bx_sz, ep, 'achiral', 'XY');
+    [XV_bin{i,5}(:,1), XV_bin{i,5}(:,2), XV_bin{i,5}(:,3)] = vecBin1(Vnorm, [bw(i) bw(i+1)], ep, 'XY');
+end
+
+
+
+%% SAVING
+
+%convergent flows
+u.Profile.points = Xu;
+u.Profile.bin = Xu_bin;
+u.Norm = unorm;
+
+%shear flows
+v.Profile.points = Xv;
+v.Profile.bin = Xv_bin;
+v.Norm = vnorm;
+v.Lambda = lambda_v;
+
+%flow direction
+Ang.Profile.points = Xang;
+Ang.Profile.bin = Xang_bin;
+Ang.Lambda = lambda_tv;
+
+%velocity norm
+V.Profile.points = XV;
+V.Profile.bin = XV_bin;
+V.Norm = Vnorm;
+
+data_piv = struct(...
+    'u', u, ...
+    'v', v, ...
+    'Ang', Ang, ...
+    'V', V);
+
+save([pathname,'\analysis\data_piv.mat'],'data_piv');
+
+%% FIGURES
+% /!\ TO CORRECT
+
 switch fig
     case 'on'
+        
+        %profiles
+        figure(11); clf;
+        for i = 1:size(Xu_bin,1)
+            subplot(4,4,i); hold on;
+            profile = Xu_bin{i,3};
+            if isempty(profile)==1
+                continue
+            end
+            plot(profile(:,1),profile(:,2),'ko');
+            xbin = Xu_bin{i,4}(:,1); ybin = Xu_bin{i,4}(:,2); sbin = Xu_bin{i,4}(:,3);
+            plot(xbin,ybin,'rs-','MarkerSize',10);
+            title(Xu_bin{i,1}); xlabel('position in stripes'); ylabel('cellular angle');
+        end
+        
+        %profiles (normalized)
+        figure(12); clf;
+        cmap = colormap(cool(size(Xu_bin,1)));
+        for i = 1:size(Xu_bin,1)
+            if isempty(Xu_bin{i,4})==1
+                continue
+            end
+            subplot(1,2,1);  hold on;
+            xbin = Xu_bin{i,4}(:,1); ybin = Xu_bin{i,4}(:,2);
+            plot(xbin,ybin,'o-','Color',cmap(i,:));
+            title('normalized profiles'); xlabel('X/w'); ylabel('velocity (um/hr)');
+            axis([-600 600 -10 10]); axis square;
+            subplot(1,2,2);  hold on;
+            xbin = Xu_bin{i,5}(:,1); ybin = Xu_bin{i,5}(:,2);
+            plot(xbin,ybin,'o-','Color',cmap(i,:));
+            title('normalized profiles'); xlabel('X/w'); ylabel('velocity (um/hr)');
+            axis([-0.5 0.5 -10 10]); axis square;
+        end
+        
+        %norm
+        figure(13); clf; hold on;
+        plot(unorm(:,1), unorm(:,2), 'ko');
+        unorm_bin = reshape([Xu_bin{:,6}]',3,[]);
+        unorm_bin(:,find(prod(isnan(unorm_bin),1)==1))=[];
+        plot(unorm_bin(1,:), unorm_bin(2,:), 'ro-', 'MarkerSize', 10);
+        axis([0 1200 0 10]);
+        title('convergent flow norm2'), xlabel('stripe width (um)'); ylabel('velocity (um^2/hr^2)');
+        legend({'all points', 'binned'});
+        
+        figure(11); savefig([pathname,'\analysis\figures\',exp_name,'_convergent_flow_profile_allpoints.fig']);
+        figure(12); savefig([pathname,'\analysis\figures\',exp_name,'_convergent_flow_profile_normalized.fig']);
+        figure(13); savefig([pathname,'\analysis\figures\',exp_name,'_convergent_flow_norm.fig']);
+        
+        %profiles
+        figure(21); clf;
+        for i = 1:size(Xv_bin,1)
+            subplot(4,4,i); hold on;
+            profile = Xv_bin{i,3};
+            if isempty(profile)==1
+                continue
+            end
+            plot(profile(:,1),profile(:,2),'ko');
+            xbin = Xv_bin{i,4}(:,1); ybin = Xv_bin{i,4}(:,2); sbin = Xv_bin{i,4}(:,3);
+            plot(xbin,ybin,'rs-','MarkerSize',10);
+            title(Xv_bin{i,1}); xlabel('position in stripes'); ylabel('cellular angle');
+        end
+        
+        %profiles (normalized)
+        figure(22); clf;
+        cmap = colormap(cool(size(Xv_bin,1)));
+        for i = 1:size(Xv_bin,1)
+            if isempty(Xv_bin{i,5})==1
+                continue
+            end
+            subplot(1,2,1); hold on;
+            xbin = Xv_bin{i,4}(:,1); ybin = Xv_bin{i,4}(:,2);
+            plot(xbin,ybin,'o-','Color',cmap(i,:));
+            title('profiles'); xlabel('X/w'); ylabel('velocity (um/hr)');
+            axis([-600 600 -10 10]); axis square;
+            
+            subplot(1,2,2); hold on;
+            xbin = Xv_bin{i,5}(:,1); ybin = Xv_bin{i,5}(:,2);
+            plot(xbin,ybin,'o-','Color',cmap(i,:));
+            title('normalized profiles'); xlabel('X/w'); ylabel('velocity (um/hr)');
+            axis([-0.5 0.5 -10 10]); axis square;
+        end
+        
+        %norm
+        figure(23); clf; hold on;
+        plot(vnorm(:,1), vnorm(:,2), 'ko');
+        vnorm_bin = reshape([Xv_bin{:,6}]',3,[]);
+        vnorm_bin(:,find(prod(isnan(vnorm_bin),1)==1))=[];
+        plot(vnorm_bin(1,:), vnorm_bin(2,:), 'ro-', 'MarkerSize', 10);
+        axis([0 1200 0 10]);
+        title('shear flow norm2'), xlabel('stripe width (um)'); ylabel('velocity (um^2/hr^2)');
+        legend({'all points', 'binned'});
+        
+        figure(21); savefig([pathname,'\analysis\figures\',exp_name,'_shear_flow_profile_allpoints.fig']);
+        figure(22); savefig([pathname,'\analysis\figures\',exp_name,'_shear_flow_profile_normalized.fig']);
+        figure(23); savefig([pathname,'\analysis\figures\',exp_name,'_shear_flow_norm.fig']);
+        figure(24); savefig([pathname,'\analysis\figures\',exp_name,'_shear_flow_friction_length.fig']);
         
         figure(31); clf;
         for i = 1:size(Xang_bin,1)
@@ -480,44 +519,6 @@ switch fig
         figure(31); savefig([pathname,'\analysis\figures\',exp_name,'_flow_direction_profile_allpoints.fig']);
         figure(32); savefig([pathname,'\analysis\figures\',exp_name,'_flow_direction_profile_normalized.fig']);
         figure(33); savefig([pathname,'\analysis\figures\',exp_name,'_flow_direction_coherence_length.fig']);
-        
-    otherwise
-        0;
-end
-
-%% 4. velocity norm
-
-% profile
-XV = NaN*ones(100,2,length(d));
-Vnorm = NaN*ones(length(d),2);
-for i = 1:length(d)
-    clc; disp([num2str(i),'/',num2str(length(d))]);
-    if d(i).isdir==0
-        
-        load([pathname,'\analysis\velocity data\',strrep(d(i).name,'.tif','.mat')]);
-        
-        px2mic = setpx2mic(d(i).name,'PIV');
-        x = (data.Grid - data.Width/2) * px2mic;
-        XV(1:length(x),1,i) = x;
-        XV(1:length(x),2,i) = data.Profile.V  * px2mic;
-        
-        Vnorm(i,1) = data.Width * px2mic;
-        Vnorm(i,2) = data.Norm2.XYT.V * px2mic^2;
-        
-    end
-end
-XV(:,:,find(prod(prod(isnan(XV(:,:,:)),2))==1)) = [];
-Vnorm(find(prod(isnan(Vnorm(:,:)),2)==1),:) = [];
-
-for i = 1:length(bw)-1
-    XV_bin{i,1} = {[num2str(bw(i)),' - ',num2str(bw(i+1))]};
-    [XV_bin{i,2}, XV_bin{i,3}, XV_bin{i,4}] = vecBin2(XV, [bw(i) , bw(i+1)], bx_sz, ep, 'achiral', 'XY');
-    [XV_bin{i,5}(:,1), XV_bin{i,5}(:,2), XV_bin{i,5}(:,3)] = vecBin1(Vnorm, [bw(i) bw(i+1)], ep, 'XY');
-end
-
-%figure
-switch fig
-    case 'on'
         
         figure(41); clf;
         for i = 1:size(XV_bin,1)
@@ -560,35 +561,3 @@ switch fig
     otherwise
         0;
 end
-
-
-%% 5. saving
-
-%convergent flows
-u.Profile.points = Xu;
-u.Profile.bin = Xu_bin;
-u.Norm = unorm;
-
-%shear flows
-v.Profile.points = Xv;
-v.Profile.bin = Xv_bin;
-v.Norm = vnorm;
-v.Lambda = lambda_v;
-
-%flow direction
-Ang.Profile.points = Xang;
-Ang.Profile.bin = Xang_bin;
-Ang.Lambda = lambda_tv;
-
-%velocity norm
-V.Profile.points = XV;
-V.Profile.bin = XV_bin;
-V.Norm = Vnorm;
-
-data_piv = struct(...
-    'u', u, ...
-    'v', v, ...
-    'Ang', Ang, ...
-    'V', V);
-
-save([pathname,'\analysis\data_piv.mat'],'data_piv');
